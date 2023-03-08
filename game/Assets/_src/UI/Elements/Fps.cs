aba1a13ddc3191b69247bc8035a96d12dedaaf1e
Timur Fesina <merlin.fs@gmail.com>
2023-03-08 08:37:09 +0100

    fps label

--- game/Assets/_src/UI/Elements/Fps.cs
+++ game/Assets/_src/UI/Elements/Fps.cs
@@ -9,12 +9,12 @@ namespace Game.UI.Elements
     {
         public new class UxmlFactory : UxmlFactory<FpsLabel, UxmlTraits> { }
 
-        private float avgFramerate;
-        string display = "{0} FPS";
-        int[] m_FpsBuffer = new int[200];
+        const string display = "{0} FPS";
+        const int m_FpsRange = 30;
+
+        int[] m_FpsBuffer = new int[m_FpsRange];
         int m_FpsBufferIndex;
-        int m_FpsRange = 200;
-        float AverageFPS;
+        float m_AverageFPS;
 
         public new class UxmlTraits : Label.UxmlTraits
         {
@@ -25,13 +25,11 @@ namespace Game.UI.Elements
                 (ve as FpsLabel).schedule.Execute(state =>
                 {
                     label.m_FpsBuffer[label.m_FpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
-                    if (label.m_FpsBufferIndex >= label.m_FpsRange)
+                    if (label.m_FpsBufferIndex >= m_FpsRange)
                         label.m_FpsBufferIndex = 0;
                     CalculateFPS(label);
-
-                    label.text = string.Format(label.display, label.AverageFPS);
-                    //float timelapse = Mathf.Round(1f / Time.smoothDeltaTime);
-                    //
+                   
+                    label.text = string.Format(display, MathF.Round(label.m_AverageFPS));
                 }).Every(100);
             }
 
@@ -40,20 +38,16 @@ namespace Game.UI.Elements
                 int sum = 0;
                 int highest = 0;
                 int lowest = int.MaxValue;
-                for (int i = 0; i < label.m_FpsRange; i++)
+                for (int i = 0; i < m_FpsRange; i++)
                 {
                     int fps = label.m_FpsBuffer[i];
                     sum += fps;
                     if (fps > highest)
-                    {
                         highest = fps;
-                    }
                     if (fps < lowest)
-                    {
                         lowest = fps;
-                    }
                 }
-                label.AverageFPS = (float)sum / label.m_FpsRange;
+                label.m_AverageFPS = (float)sum / m_FpsRange;
             }
         }
     }
