66c7cf733e0d4c0055334c5b8b20b06762591c75
Timur Fesina <merlin.fs@gmail.com>
2023-07-23 08:40:19 +0200

    remove graphtools (temp)

--- game/Assets/Plugins/com.unity.graphtools.foundation/Editor/EditorCommon/Utility/AssetHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityEditor.GraphToolsFoundation.Overdrive
-{
-    static class AssetHelper
-    {
-        //internal const string AssetPath = "Packages/com.unity.graphtools.foundation/Editor/";
-        internal const string AssetPath = "Assets/Plugins/com.unity.graphtools.foundation/Editor/";
-        //\Assets\Plugins\com.unity.graphtools.foundation@0.11.2-preview
-    }
-}
--- game/Assets/Plugins/com.unity.graphtools.foundation/Tests/Editor/Models/CompatibilityTests/CompatibilityTestGraphModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEditor.GraphToolsFoundation.Overdrive.BasicModel;
-
-namespace UnityEditor.GraphToolsFoundation.Overdrive.Tests.Models
-{
-    public class CompatibilityTestGraphModel : GraphModel
-    {
-    }
-}
